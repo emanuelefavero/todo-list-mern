f058ecd4b0db40addd7145cf9e834f0425d82d32
emanuelefavero <emafaux89@gmail.com>
2024-03-05 08:00:59 +0100

    add - addItem and deleteItem in itemController

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -10,3 +10,27 @@ exports.getItems = async (req, res) => {
     res.status(500).json({ message: err.message })
   }
 }
+
+// Add an item
+exports.addItem = async (req, res) => {
+  const item = new Item({
+    name: req.body.name,
+  })
+
+  try {
+    const newItem = await item.save()
+    res.status(201).json(newItem)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
+}
+
+// Delete an item
+exports.deleteItem = async (req, res) => {
+  try {
+    await Item.findByIdAndDelete(req.params.id)
+    res.status(200).json({ message: 'Item deleted' })
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+}
