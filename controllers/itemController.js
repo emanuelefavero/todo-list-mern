00172e96c062f8dd54ccfcaca29ae3deb6669495
emanuelefavero <emafaux89@gmail.com>
2024-03-05 05:29:38 +0100

    add home page

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -5,7 +5,7 @@ exports.getItems = async (req, res) => {
   try {
     const items = await Item.find()
 
-    res.json(items)
+    res.status(200).json(items)
   } catch (err) {
     res.status(500).json({ message: err.message })
   }
